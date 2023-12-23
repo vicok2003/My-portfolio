964b379a988a999fc7f4d3963c85835403222e03
vicok2003 <vicok2003@gmail.com>
2023-12-23 02:51:55 +0100

    third commit

--- index.js
+++ index.js
@@ -8,64 +8,32 @@ menuIcon.addEventListener('click', () => {
 });
 
 
-// let sections = document.querySelectorAll('section');
-// let navlinks = document.querySelectorAll('header nav a');
-
-
-// window.onscroll = () =>{
-//     sections.forEach(sec =>{
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
-
-//         if(top >= offset && top < offset + height) {
-//             navlinks.forEach(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-//             });
-//         }
-//     });
-
-//     let header = document.querySelector('header');
-
-//     header.classList.toggle('sticky', window.scrollY > 100); 
-
-//      menuIcon.classList.remove('bx-x')
-//     navbar.classList.remove('act')
-// };
-
-let navLinks = document.querySelectorAll('header nav a');
 let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
-window.addEventListener('scroll', () => {
-    let scrollY = window.scrollY;
 
-    sections.forEach((section) => {
-        let sectionOffset = section.offsetTop - 150;
-        let sectionHeight = section.offsetHeight;
-        let sectionId = section.getAttribute('id');
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if (scrollY >= sectionOffset && scrollY < sectionOffset + sectionHeight) {
-            navLinks.forEach((link) => {
-                link.classList.remove('active');
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-            document.querySelector(`header nav a[href="#${sectionId}"]`).classList.add('active');
         }
     });
 
     let header = document.querySelector('header');
-    header.classList.toggle('sticky', scrollY > 100);
 
-    // Close the mobile menu when scrolling
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('act');
-});
+    header.classList.toggle('sticky', window.scrollY > 100); 
 
-
-
-
-
+     menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('act')
+};
 
 
 
